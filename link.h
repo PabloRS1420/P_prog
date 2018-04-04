5c650b823b1ff238f2e2ddf9d41120070914486c
PabloRS1420 <36885631+PabloRS1420@users.noreply.github.com>
2018-04-04 21:41:14 +0200

    Update link.h

--- link.h
+++ link.h
@@ -18,25 +18,116 @@
 
 struct _Link Link;
 
+/**
+  * @description Reserves memory for a Link* initialising the struct.
+  * @input id
+  * @output Link*
+  * @author Pablo Rosales Sánchez
+  */
 Link* link_create(Id id);
+
+/**
+  * @description Frees all the memory reserved.
+  * @input Link*
+  * @output OK if everything went well or ERROR if not.
+  * @author Pablo Rosales Sánchez
+  */
 STATUS link_destroy(Link* link);
 
+/**
+  * @description Sets the name of the link entered.
+  * @input Link*, char*
+  * @output OK if everything went well or ERROR if not.
+  * @author Pablo Rosales Sánchez
+  */
 STATUS link_set_name(Link* link, char* name);
+
+/**
+  * @description Gets the name of the link entered.
+  * @input Link*
+  * @output char*
+  * @author Pablo Rosales Sánchez
+  */
 const char* link_get_name(Link* link);
 
+/**
+  * @description Sets the id of the id of the link entered.
+  * @input Link*, Id
+  * @output OK if everything went well or ERROR if not.
+  * @author Pablo Rosales Sánchez
+  */
 STATUS link_set_id(Link* link, Id id);
+
+/**
+  * @description Gets the id of the id of the link entered.
+  * @input Link*
+  * @output Id
+  * @author Pablo Rosales Sánchez
+  */
 Id link_get_id(Link* link);
 
+/**
+  * @description Sets the id of the first space of the link entered.
+  * @input Link*, Id
+  * @output OK if everything went well or ERROR if not.
+  * @author Pablo Rosales Sánchez
+  */
 STATUS link_set_spaceL1(Link* link, Id id);
+
+/**
+  * @description Gets the id of the first space of the link entered.
+  * @input Link*
+  * @output Id
+  * @author Pablo Rosales Sánchez
+  */
 Id link_get_spaceL1(Link* link);
 
+/**
+  * @description Sets the id of the second space of the link entered.
+  * @input Link*, Id
+  * @output OK if everything went well or ERROR if not.
+  * @author Pablo Rosales Sánchez
+  */
 STATUS link_set_spaceL2(Link* link, Id id);
+
+/**
+  * @description Gets the id of the second space of the link entered.
+  * @input Link*
+  * @output Id
+  * @author Pablo Rosales Sánchez
+  */
 Id link_get_spaceL2(Link* link);
 
+/**
+  * @description Sets the id of the status of the link entered.
+  * @input Link*, Id
+  * @output OK if everything went well or ERROR if not.
+  * @author Pablo Rosales Sánchez
+  */
 STATUS link_set_statusL(Link* link, Id id);
+
+/**
+  * @description Gets the id of the status of the link entered.
+  * @input Link*
+  * @output Id
+  * @author Pablo Rosales Sánchez
+  */
 Id link_get_statusL(Link* link);
 
+/**
+  * @description Copies all the information of a the link entered into another one.
+  * @input Link*
+  * @output Link*
+  * @author Pablo Rosales Sánchez
+  */
 Link* link_copy(const Link* link);
-STATUS link_print(FILE* f, Link* link);
+
+/**
+  * @description Prints the relevant data of the link.
+  * @input Link*
+  * @output OK if everything went well or ERROR if not.
+  * @author Pablo Rosales Sánchez
+  */
+STATUS link_print(Link* link);
 
 #endif

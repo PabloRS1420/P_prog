6aeefb32ac4cafb7441d10a4ab902efc17144c4f
PabloRS1420 <36885631+PabloRS1420@users.noreply.github.com>
2018-04-12 12:05:42 +0200

    Update space.c

--- space.c
+++ space.c
@@ -15,13 +15,16 @@
 struct _Space {
   Id id;
   char name[WORD_SIZE + 1];
-  Id north;
-  Id south;
-  Id east;
-  Id west;
+  Link* north;
+  Link* south;
+  Link* east;
+  Link* west;
+  Link* up;
+  Link* down;
   Set* objects;
   char* gdesc[3];
   char description[WORD_SIZE];
+  ILLUMINATION_SPACE illumination;
 };
 
 /****************************/
@@ -44,10 +47,20 @@ Space* space_create(Id id) {
 
   new_space->name[0] = '\0';
 
-  new_space->north = NO_ID;
-  new_space->south = NO_ID;
-  new_space->east = NO_ID;
-  new_space->west = NO_ID;
+  new_space->north = NULL;
+  new_space->north = link_create(NO_ID);
+  new_space->south = NULL;
+  new_space->south = link_create(NO_ID);
+  new_space->east = NULL;
+  new_space->east = link_create(NO_ID);
+  new_space->west = NULL;
+  new_space->west = link_create(NO_ID);
+  new_space->up = NULL;
+  new_space->up = link_create(NO_ID);
+  new_space->down = NULL;
+  new_space->down = link_create(NO_ID);
+  
+  new_space->illumination = ILLUMINATED;
 
   new_space->objects = set_create();
 
@@ -90,35 +103,75 @@ STATUS space_set_name(Space* space, char* name) {
   return OK;
 }
 
-STATUS space_set_north(Space* space, Id id) {
-  if (!space || id == NO_ID) {
+STATUS space_set_north(Space* space, Id idL, Id spaceL1, Id spaceL2, char nameL, LINK_STATUS statusL) {
+  if (!space || idL == NO_ID || spaceL1 == NO_ID || spaceL2 == NO_ID || nameL == NULL || statusL < 0 || statusL > 1) {
     return ERROR;
   }
-  space->north = id;
+  space->north->idL = idL;
+  space->north->spaceL1 = spaceL1;
+  space->north->spaceL2 = spaceL2;
+  strcpy(space->north->nameL, nameL);
+  space->north->statusL = statusL;
   return OK;
 }
 
-STATUS space_set_south(Space* space, Id id) {
-  if (!space || id == NO_ID) {
+STATUS space_set_south(Space* space, Id idL, Id spaceL1, Id spaceL2, char nameL, LINK_STATUS statusL) {
+  if (!space || idL == NO_ID || spaceL1 == NO_ID || spaceL2 == NO_ID || nameL == NULL || statusL < 0 || statusL > 1) {
     return ERROR;
   }
-  space->south = id;
+  space->south->idL = idL;
+  space->south->spaceL1 = spaceL1;
+  space->south->spaceL2 = spaceL2;
+  strcpy(space->south->nameL, nameL);
+  space->south->statusL = statusL;
   return OK;
 }
 
-STATUS space_set_east(Space* space, Id id) {
-  if (!space || id == NO_ID) {
+STATUS space_set_east(Space* space, Id idL, Id spaceL1, Id spaceL2, char nameL, LINK_STATUS statusL) {
+  if (!space || idL == NO_ID || spaceL1 == NO_ID || spaceL2 == NO_ID || nameL == NULL || statusL < 0 || statusL > 1) {
     return ERROR;
   }
-  space->east = id;
+  space->east->idL = idL;
+  space->east->spaceL1 = spaceL1;
+  space->east->spaceL2 = spaceL2;
+  strcpy(space->east->nameL, nameL);
+  space->east->statusL = statusL;
   return OK;
 }
 
-STATUS space_set_west(Space* space, Id id) {
-  if (!space || id == NO_ID) {
+STATUS space_set_west(Space* space, Id idL, Id spaceL1, Id spaceL2, char nameL, LINK_STATUS statusL) {
+  if (!space || idL == NO_ID || spaceL1 == NO_ID || spaceL2 == NO_ID || nameL == NULL || statusL < 0 || statusL > 1) {
     return ERROR;
   }
-  space->west = id;
+  space->west->idL = idL;
+  space->west->spaceL1 = spaceL1;
+  space->west->spaceL2 = spaceL2;
+  strcpy(space->west->nameL, nameL);
+  space->west->statusL = statusL;
+  return OK;
+}
+
+STATUS space_set_up(Space* space, Id idL, Id spaceL1, Id spaceL2, char nameL, LINK_STATUS statusL) {
+  if (!space || idL == NO_ID || spaceL1 == NO_ID || spaceL2 == NO_ID || nameL == NULL || statusL < 0 || statusL > 1) {
+    return ERROR;
+  }
+  space->up->idL = idL;
+  space->up->spaceL1 = spaceL1;
+  space->up->spaceL2 = spaceL2;
+  strcpy(space->up->nameL, nameL);
+  space->up->statusL = statusL;
+  return OK;
+}
+
+STATUS space_set_down(Space* space, Id idL, Id spaceL1, Id spaceL2, char nameL, LINK_STATUS statusL) {
+  if (!space || idL == NO_ID || spaceL1 == NO_ID || spaceL2 == NO_ID || nameL == NULL || statusL < 0 || statusL > 1) {
+    return ERROR;
+  }
+  space->down->idL = idL;
+  space->down->spaceL1 = spaceL1;
+  space->down->spaceL2 = spaceL2;
+  strcpy(space->down->nameL, nameL);
+  space->down->statusL = statusL;
   return OK;
 }
 
@@ -151,6 +204,15 @@ STATUS space_set_description(Space* space, char* des){
   return OK;
 }
 
+STATUS space_set_illumination(Space* space, ILLUMINATION_SPACE illumination){
+  if (!space || illumination < 0 || illumination > 1) {
+    return ERROR;
+  }
+  
+  space->illumination = illumination;
+  return OK;
+}
+
 const char * space_get_name(Space* space) {
   if (!space) {
     return NULL;
@@ -165,32 +227,46 @@ Id space_get_id(Space* space) {
   return space->id;
 }
 
-Id space_get_north(Space* space) {
+Link* space_get_north(Space* space) {
   if (!space) {
     return NO_ID;
   }
   return space->north;
 }
 
-Id space_get_south(Space* space) {
+Link* space_get_south(Space* space) {
   if (!space) {
     return NO_ID;
   }
   return space->south;
 }
 
-Id space_get_east(Space* space) {
+Link* space_get_east(Space* space) {
   if (!space) {
     return NO_ID;
   }
   return space->east;
 }
 
-Id space_get_west(Space* space) {
+Link* space_get_west(Space* space) {
   if (!space) {
     return NO_ID;
   }
   return space->west;
+}
+
+Link* space_get_up(Space* space) {
+  if (!space) {
+    return NO_ID;
+  }
+  return space->up;
+}
+
+Link* space_get_down(Space* space) {
+  if (!space) {
+    return NO_ID;
+  }
+  return space->down;
 }
 
 Set* space_get_object_set(Space* space) {
@@ -214,41 +290,51 @@ const char* space_get_description(Space* space){
   return space->description;
 }
 
+ILLUMINATION_SPACE space_get_illumination(Space* space){
+  if (!space){
+    return NULL;
+  }
+  return space->illumination;
+}
+
 STATUS space_print(Space* space) {
   Id idaux = NO_ID;
+  STATUS status = OK
 
   if (!space) {
     return ERROR;
   }
 
   fprintf(stdout, "--> Space (Id: %ld; Name: %s)\n", space->id, space->name);
-
-  idaux = space_get_north(space);
-  if (NO_ID != idaux) {
-    fprintf(stdout, "---> North link: %ld.\n", idaux);
-  } else {
-    fprintf(stdout, "---> No north link.\n");
+  fprintf(stdout, "--> North: ");
+  status = link_print(stdout, space->north);
+  if (status != OK) {
+    fprintf(stdout, "---> No link.\n");
   }
-
-  idaux = space_get_south(space);
-  if (NO_ID != idaux) {
-    fprintf(stdout, "---> South link: %ld.\n", idaux);
-  } else {
-    fprintf(stdout, "---> No south link.\n");
+  fprintf(stdout, "--> South: ");
+  status = link_print(stdout, space->south);
+  if (status != OK) {
+    fprintf(stdout, "---> No link.\n");
   }
-
-  idaux = space_get_east(space);
-  if (NO_ID != idaux) {
-    fprintf(stdout, "---> East link: %ld.\n", idaux);
-  } else {
-    fprintf(stdout, "---> No east link.\n");
+  fprintf(stdout, "--> East: ");
+  status = link_print(stdout, space->east);
+  if (status != OK) {
+    fprintf(stdout, "---> No link.\n");
   }
-
-  idaux = space_get_west(space);
-  if (NO_ID != idaux) {
-    fprintf(stdout, "---> West link: %ld.\n", idaux);
-  } else {
-    fprintf(stdout, "---> No west link.\n");
+  fprintf(stdout, "--> West: ");
+  status = link_print(stdout, space->west);
+  if (status != OK) {
+    fprintf(stdout, "---> No link.\n");
+  }
+  fprintf(stdout, "--> Up: ");
+  status = link_print(stdout, space->up);
+  if (status == OK) {
+    fprintf(stdout, "---> No link.\n");
+  }
+  fprintf(stdout, "--> Down: ");
+  status = link_print(stdout, space->down);
+  if (status == OK) {
+    fprintf(stdout, "---> No link.\n");
   }
 
   fprintf(stdout, "---> Objects located in the Space:\n");
@@ -260,7 +346,13 @@ STATUS space_print(Space* space) {
   fprintf(stdout, "%s\n",space_get_gdesc(space)[0]);
   fprintf(stdout, "%s\n",space_get_gdesc(space)[1]);
   fprintf(stdout, "%s\n",space_get_gdesc(space)[2]);
-
-
+  
+  if (space_get_illumination(space)==ILLUMINATED) {
+     fprintf(stdout, "---> Space illuminated");
+  }
+  else {
+    fprintf(stdout, "---> Space not illuminated");
+  }
+ 
   return OK;
 }
